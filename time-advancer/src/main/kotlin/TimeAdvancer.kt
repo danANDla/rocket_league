b4f7f6e41141a6d0ba32f7f7ae63259310c53a3b
archandla <d.a.n.202021@gmail.com>
2025-12-09 05:00:56 +0300

    generating cpu clock, and condition for turning engine off

--- time-advancer/src/main/kotlin/TimeAdvancer.kt
+++ time-advancer/src/main/kotlin/TimeAdvancer.kt
@@ -8,6 +8,8 @@ fun main() {
 
     var t = 0.0
     val dt = 0.1
+    val CPUclockFactor = 100
+    var ticksBeforeClock = 0
 
     println("[TIME] Orchestrator started")
 
@@ -15,6 +17,13 @@ fun main() {
         t += dt
         val msg = Json.encodeToString(TimeTick(t, dt))
         nc.publish("time.tick", msg.toByteArray())
+        ticksBeforeClock += 1
+        if(ticksBeforeClock == CPUclockFactor) {
+            ticksBeforeClock = 0
+            nc.publish("100ms", msg.toByteArray())
+        }
+
         Thread.sleep((dt * 1000).toLong())
+
     }
 }
